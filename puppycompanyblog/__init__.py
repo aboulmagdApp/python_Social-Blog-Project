a3c799543034ffa59671d92560cd6202341afc02
aboulmagdApp <aboulmagd@live.com>
2019-08-07 02:30:31 +0300

    Error Pages

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
 
 app = Flask(__name__)
 
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
 
