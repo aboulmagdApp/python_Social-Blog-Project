7a214a7b864ac1e9828e43fc6d6d540b425b14a5
aboulmagdApp <aboulmagd@live.com>
2019-08-12 18:31:41 +0300

    add forms,views  and app.py

--- puppycompanyblog/models.py
+++ puppycompanyblog/models.py
@@ -1,3 +1,4 @@
+#models.py
 from puppycompanyblog import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
@@ -7,12 +8,12 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(user_id)
 
-class User(db.Model):
-    
-    __tablenamme__ = 'users'
+class User(db.Model,UserMixin):
 
-    id = db.Column(db.Interger, primary_key=True)
-    profile_image = db.Column(db.String(20), nullable=False,default='default_profile.png')
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key=True)
+    profile_image = db.Column(db.String(64),nullable=False,default='default_profile.jpg')
     email = db.Column(db.String(64),unique=True,index=True)
     username = db.Column(db.String(64),unique=True,index=True)
     password_hash = db.Column(db.String(128))
@@ -23,18 +24,20 @@ class User(db.Model):
         self.email = email
         self.username = username
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
-    
+
     def __repr__(self):
         return f"Username {self.username}"
+
+
 class BlogPost(db.Model):
-    
+
     users = db.relationship(User)
 
     id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=Flase)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
 
     date = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
     title = db.Column(db.String(140),nullable=False)
@@ -46,4 +49,4 @@ class BlogPost(db.Model):
         self.user_id = user_id
 
     def __repr__(self):
-        return f"post Id:{self.id} --- Date: {self.date} --- {self.title}"
+        return f"Post ID: {self.id} -- Date: {self.date} --- {self.title}"
